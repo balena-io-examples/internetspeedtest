a59d64c80441920cbba0884f535d2aad2c65decb
Phil Wilson <phil@balena.io>
2020-06-12 13:47:39 +0100

    MVP

--- grafana/entry.sh
+++ grafana/entry.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ ! -d "/data/grafana/plugins/grafana-image-renderer" ]
+then
+    grafana-cli --pluginsDir "/data/grafana/plugins" plugins install grafana-image-renderer
+    sleep 10
+fi
+
 /usr/src/app/api.sh &
-exec grafana-server -homepath /usr/share/grafana
+exec grafana-server -homepath /usr/share/grafana -config /usr/src/app/grafana.ini
