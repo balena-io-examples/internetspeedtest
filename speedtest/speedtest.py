64b31f21bf478802638500a91d5f6c6329c28e9c
Oriol Piera <oriol.piera@somenergia.coop>
2024-12-20 08:17:32 +0100

    Add ping test

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -24,6 +24,44 @@ class speedtest():
         print ("Server = " + str(result['server']))
         return result
 
+    def ping_adreca(adreca, duracio_segons):
+        temps_final = time.time() + duracio_segons
+        paquets_enviats = 0
+        paquets_perduts = 0
+        temps_total = 0
+        respostes_valides = 0
+
+        while time.time() < temps_final:
+            try:
+                # Executa el ping una vegada
+                resultat = subprocess.run(
+                    ["ping", "-c", "1", adreca],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True
+                )
+                paquets_enviats += 1
+
+                # Busca el temps de resposta en el resultat
+                if resultat.returncode == 0:
+                    match = re.search(r'time=(\d+\.\d+) ms', resultat.stdout)
+                    if match:
+                        temps_total += float(match.group(1))
+                        respostes_valides += 1
+                else:
+                    paquets_perduts += 1
+            except Exception as e:
+                print(f"Error en executar el ping: {e}")
+                paquets_perduts += 1
+
+            time.sleep(1)  # Espera 1 segon abans del següent ping
+
+        # Calcula la mitjana de temps
+        temps_mitja = temps_total / respostes_valides if respostes_valides > 0 else 0
+
+        return paquets_enviats, paquets_perduts, temps_mitja
+
+
 speedtest = speedtest()
 frequency = os.environ.get('FREQUENCY') or 3600
 broker_address = os.environ.get('MQTT_BROKER') or "localhost"
@@ -33,6 +71,7 @@ client = mqtt.Client("1")
 while True:
     client.connect(broker_address)
     result = speedtest.test()
+    paquets_enviats, paquets_perduts, temps_mitja = speedtest.ping_adreca('www.google.com', int(frequency))
     json_body = [
         {
             "measurement": "download",
@@ -43,7 +82,10 @@ while True:
                 "latency": float(result['ping']['latency']),
                 "jitter": float(result['ping']['jitter']),
                 "interface": str(result['interface']['name']),
-                "server": str(result['server']['host'])
+                "server": str(result['server']['host']),
+                "paquets_enviats": str(paquets_enviats),
+                "paquets_perduts": str(paquets_perduts),
+                "temps_mitja": str(temps_mitja)
             }
         }
     ]
@@ -53,7 +95,7 @@ while True:
     if msg_info.is_published() == False:
             msg_info.wait_for_publish()
     client.disconnect()
-    time.sleep(int(frequency))
+    #time.sleep(int(frequency))
 
 
 
